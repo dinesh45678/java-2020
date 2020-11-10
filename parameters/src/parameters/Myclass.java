2a3ac93474082c2e9372d2db2263f317e95c8cb0
sekhar <sekhar@DESKTOP-SEKHAR>
2020-11-10 16:23:43 +0530

    initia draft

---
+++ parameters/src/parameters/Myclass.java
@@ -0,0 +1,13 @@
+package parameters;
+
+public class Myclass {
+	static void myMethod(String fname) {
+	System.out.println(fname + "reference");
+	} 
+	public static void main(String[] args) {
+		myMethod("vidip");
+		myMethod("varshi");
+		myMethod("nitin");
+	}
+
+}
